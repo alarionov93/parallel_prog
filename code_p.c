363f53fb420ac86b2a8ea61b3a7e4b31f3229959
Alexandr Larionov <alarionov93@yandex.ru>
2016-05-23 23:35:08 +0500

    initial

---
+++ code_p.c
@@ -0,0 +1,37 @@
+#include "stdafx.h"
+#include <iostream>
+#include <stdlib.h>
+#include <windows.h>
+#include <stdio.h>
+#include "omp.h"
+#include "time.h"
+
+using namespace std;
+
+double f(double x) {
+	return x * x;
+}
+
+int main(){
+	SetConsoleOutputCP(1251);
+	SetConsoleCP(1251);
+
+	double a;
+	double b;
+	cout << "ввод а и b через пробел:";
+	cin >> a >> b;
+	cout << "ввод N:";
+	int n=0;
+	cin >> n;
+	double h = (b-a) / n;
+	double s = 0;
+	double start = ((double)clock()) / CLOCKS_PER_SEC;
+	#pragma omp parallel for reduction(+:s)
+	for (int i = 0; i<n; i++){
+		s += f(a + i*h)*h;
+	}
+
+	cout << "Результат:" << s << endl;
+	cout << "Быстродействие:" << ((double)clock()) / CLOCKS_PER_SEC  - start << endl;
+	system("pause");
+}
